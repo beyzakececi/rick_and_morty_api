7eaa0a7158c1d5769fa979750bf1d515c4344b76
beyzakececi <beyzakececi.35@gmail.com>
2024-07-12 03:53:44 +0300

    Add switch button to show characters that are alive

--- lib/features/character/view/screens/character_list_screen.dart
+++ lib/features/character/view/screens/character_list_screen.dart
@@ -1,37 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-
 import '../../../locations/viewmodel/data_provider.dart';
 import '../../../shared/view/widgets/search_bar_custom.dart';
 import '../widgets/character_card.dart';
 
-class CharacterListScreen extends StatelessWidget {
+class CharacterListScreen extends StatefulWidget {
   const CharacterListScreen({Key? key}) : super(key: key);
+
+  @override
+  _CharacterListScreenState createState() => _CharacterListScreenState();
+}
+
+class _CharacterListScreenState extends State<CharacterListScreen> {
+  bool showAliveOnly = false;
 
   @override
   Widget build(BuildContext context) {
     final dataProvider = Provider.of<DataProvider>(context);
+    final filteredCharacters = showAliveOnly
+        ? dataProvider.characters.where((char) => char.status.toLowerCase() == 'alive').toList()
+        : dataProvider.characters;
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(50.0),
-        child: SearchBarCustom(dataProvider: dataProvider),
+        child: Row(
+          children: [
+            Expanded(
+              child: SearchBarCustom(dataProvider: dataProvider),
+            ),
+            Switch(
+              activeColor: Colors.green,
+              value: showAliveOnly,
+              onChanged: (value) {
+                setState(() {
+                  showAliveOnly = value;
+                });
+              },
+            ),
+          ],
+        ),
       ),
       body: dataProvider.isLoading
           ? const Center(child: CircularProgressIndicator())
           : ListView.builder(
-              itemCount: dataProvider.characters.length,
-              itemBuilder: (context, index) {
-                final character = dataProvider.characters[index];
-                return GestureDetector(
-                  onTap: () {
-                    context.go('/characters/character-detail/${character.id}',
-                        extra: character);
-                  },
-                  child: CharacterCard(character: character),
-                );
-              },
-            ),
+        itemCount: filteredCharacters.length,
+        itemBuilder: (context, index) {
+          final character = filteredCharacters[index];
+          return GestureDetector(
+            onTap: () {
+              context.go('/characters/character-detail/${character.id}', extra: character);
+            },
+            child: CharacterCard(character: character),
+          );
+        },
+      ),
     );
   }
 }
